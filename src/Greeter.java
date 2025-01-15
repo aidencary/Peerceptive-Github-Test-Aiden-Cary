bf8b919692e578a76094c2b806d72b0777b90c51
Aiden Cary <ACARY@cub.uca.edu>
2025-01-14 21:20:02 -0600

    Completed program

---
+++ src/Greeter.java
@@ -0,0 +1,11 @@
+// Aiden Cary
+// Greeter.java
+// Peerceptive Github Test
+// Prints Hello World!
+
+public class Greeter {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Prints "Hello World!" with an endl at the end
+    }
+}
+
